e13c29315a9dbff2b1cbe2a3f1cb59b672481f08
MinecraftGamer38 <Aidanmlorimer@gmail.com>
2022-03-24 21:18:29 -0700

    Finished Bot!

--- Botfiles/main.js
+++ Botfiles/main.js
@@ -18,6 +18,7 @@ const transportmailer = nodemailer.createTransport({
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 const prefix = '!';
 
+
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -31,43 +32,52 @@ client.on('messageCreate', (msg) => {
 
     console.log(`Command:${cmd} Arguments:${args}`);
 
-    //read mailing list
-    const MailingList = fs.readFileSync('./MailingList.json').toString();
-    if (cmd == 'embed') {
+    const Emaillist = JSON.parse(fs.readFileSync('./Emaillist.json'));
+
+
+    if (cmd == 'email') {
+        if (!msg.member.permissions.has(Discord.Permissions.FLAGS.MENTION_EVERYONE)) {
+            msg.channel.send('You are not trusted to send Emails')
+            return;
+        }
         //Created the custom Embed
         const embed = new Discord.MessageEmbed()
             .setColor('#0b6b19')
             .setTitle(args[0])
             .setDescription(args[1]);
-        if (args[2] == 'mail') {
-            const embedmail = {
-                from: 'noreplytroop302@gmail.com',
-                to: MailingList,
-                subject: args[1],
-                text: args[2]
-            };
-            console.log('E-mail sending');
-            transportmailer.sendMail(embedmail, (err, info) => {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log('E-mail Sent' + info.response);
-                }
-
-            });
+        const embedmail = {
+            from: 'noreplytroop302@gmail.com',
+            to: Emaillist.toString(),
+            subject: args[0],
+            text: args[1]
         }
+        console.log('E-mail sending...');
+        transportmailer.sendMail(embedmail, (err, info) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('E-mail Sent' + info.response);
+                msg.channel.send('Email Succsesfully Sent');
+            }
+        });
         // sends the embed
         msg.channel.send({ embeds: [embed] });
         msg.delete();
     }
+
     else if (cmd == 'mailinglist') {
-        console.log(MailingList);
-        // const newmailinglist = MailingList.
-        fs.writeFile('./MailingList.json', newmailinglist, (err) => {
+        if (!args[0].includes('@' || '.') || args[0].includes(' ')) {
+            msg.channel.send('Sorry that E-mail is not valid');
+            return;
+        }
+        Emaillist[Emaillist.length++] = args[0];
+        let data = JSON.stringify(Emaillist);
+        fs.writeFile('./Emaillist.json', data, (err) => {
             if (err) {
                 console.log(err);
             }
-        });
+        })
+        msg.channel.send('Email Succsesfully Added')
     }
     else if (cmd == 'help') {
         msg.channel.send({ embeds: [HelpEmbed] });
